4204cc6d8ab0f8fb8170190383a83261f4173c6b
msanmaz <osanmaz4992@gmail.com>
2022-04-27 22:38:50 +0100

    passchecker

--- app.js
+++ app.js
@@ -23,5 +23,43 @@ app.post('/login', (req, res) => {
 
 })
 
+app.post('/signup', (req,res) => {
+  console.log('received sign up info')
+  const email = req.body.email
+  const password = req.body.password
+  const password_confirmation = req.body.password_confirmation
+
+  if (!email || !password || !password_confirmation) {
+    res.status(400).send('Missing data')
+    return
+  }
+
+  if (accountExists(email)) {
+    res.status(403).send('Account already exists')
+    return
+  }
+
+  if (password !== password_confirmation) {
+    res.status(400).send('Passwords do not match')
+    return
+  }
+  if (password.length < 8) {
+    res.status(400).send('Passwords is invalid')
+    return
+  }
+  createAccount(email, password)
+	res.status(200).send('Account created')
+
+})
+
+
+const accountExists = (email) => {
+  //here we should check if the account exists
+  return false
+}
+
+const createAccount = (email, password) => {
+  //here we should create the account
+}
 
 app.listen(3001, () => console.log('Server ready'))
